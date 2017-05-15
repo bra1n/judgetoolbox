853194a9b6a65e6e0ee7ab7e379f7a82bf3f0ea1
Steffen <steffen@baumgart.biz>
2017-05-15 13:28:43 +0200

    fixed a bug with reloading a scenario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {useHash: false})
   ],
   exports: [
     RouterModule
